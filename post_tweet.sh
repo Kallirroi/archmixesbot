290294f5eca9b76ff65fd4e24034cd6bb2a53185
Kallirroi <kallirroi.retzepi@gmail.com>
2017-04-26 10:17:53 -0400

    removed keys

--- post_tweet.sh
+++ post_tweet.sh
@@ -1,3 +1,3 @@
-python archmixesbot.py --consumer_key='OSvo5OAQPqGDQic0AwIi4G9GW' --consumer_secret='IjLVwgDrPP1xMOtOUwWfBfmQuVqoJqck5BFFQqX4GtP5dfuK0K' \
-      --access_token='794356609652768773-R0BqLWhLkHPX0fqm1mTcE8XbvNeNuml' --token_secret='L5huKmY3U9QRCkb3ad9p7p2JLE2QP2c1mhF1Jyxjeg5pp' \
+python archmixesbot.py --consumer_key='insert key' --consumer_secret='insert key' \
+      --access_token='insert key' --token_secret='insert key' \
       --source_file=tweet_list.txt --index_file=current_line.txt
